879d971c7f77b30fc419f97c53e9b86166a08984
xynor <ace.xinxuwang@gmail.com>
2025-10-28 17:20:43 +0800

    f

--- directBTC/model/sql.sql
+++ directBTC/model/sql.sql
@@ -1,5 +1,26 @@
 create schema directbtc collate utf8mb4_unicode_ci;
 use directbtc;
+
+create table bind_evm_signs
+(
+    id                 bigint unsigned auto_increment
+        primary key,
+    created_at         datetime(3)                 null,
+    updated_at         datetime(3)                 null,
+    deleted_at         datetime(3)                 null,
+    message            longtext                    not null,
+    signature          varchar(255)    default ''  null,
+    signer             varchar(255)    default ''  null,
+    btc_address        varchar(255)    default ''  null,
+    chain_id           bigint unsigned default '0' null,
+    binded_evm_address varchar(255)    default ''  null,
+    constraint evm_chain_btc
+        unique (btc_address, chain_id, binded_evm_address)
+);
+
+create index idx_bind_evm_signs_deleted_at
+    on bind_evm_signs (deleted_at);
+
 create table btc_trans
 (
     id                   bigint unsigned auto_increment
@@ -41,27 +62,11 @@ create table cursors
     chain_id     bigint unsigned default '0' null,
     address      varchar(255)                null,
     txhash       varchar(255)                null,
-    block_number bigint unsigned default '0' null
+    block_number bigint unsigned default '0' null,
+    constraint t_chainid_address
+        unique (is_btc, chain_id, address)
 );
-CREATE UNIQUE INDEX `t_chainid_address` ON `cursors`(`is_btc`,`chain_id`,`address`);
 
 create index idx_cursors_deleted_at
     on cursors (deleted_at);
-
-create table sign_data
-(
-    id            bigint unsigned auto_increment
-        primary key,
-    created_at    datetime(3)             null,
-    updated_at    datetime(3)             null,
-    deleted_at    datetime(3)             null,
-    message       json                    not null,
-    signature     varchar(255) default '' null,
-    sign_type     varchar(255) default '' null,
-    signer        varchar(255) default '' null,
-    btc_tran_hash varchar(255) default '' null
-);
-
-create index idx_sign_data_deleted_at
-    on sign_data (deleted_at);
 
