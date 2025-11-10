7535fb3399c747d28790fb4b381b579cc0988c99
xynor <ace.xinxuwang@gmail.com>
2025-11-10 11:56:36 +0800

    f

--- directBTC/api/internal/handler/routes.go
+++ directBTC/api/internal/handler/routes.go
@@ -32,7 +32,7 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 		[]rest.Route{
 			{
 				Method:  http.MethodPost,
-				Path:    "/btc/address/isTrial",
+				Path:    "/btc/address/trialComplete",
 				Handler: GetBtcAddressIsTrialHandler(serverCtx),
 			},
 		},
